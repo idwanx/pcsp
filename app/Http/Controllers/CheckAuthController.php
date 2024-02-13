cdf394240344ffd8e270fb6efb19b5305e44b3e8
idwan <idwanx@gmail.com>
2024-02-14 00:52:54 +0800

    wip

--- app/Http/Controllers/CheckAuthController.php
+++ app/Http/Controllers/CheckAuthController.php
@@ -20,12 +20,12 @@ class CheckAuthController extends Controller
 
         $tahunPemilu = Pemilu::max('tahun');
 
-        return dd(($roleUser->role_id == "1"));
+        // return dd(($roleUser->role_id == "1"));
 
-        if($roleUser->role_id === "1") {
-            return Redirect::route('dashboard_admin');
+        if($roleUser->role_id == "1") {
+            return dd('ke admin');
         } else {
-            return Redirect::route('dashboard_user', ['partai' => $roleUser->alias, 'tahun' => $tahunPemilu]);
+            return dd('ke user');
         }
     }
 }
