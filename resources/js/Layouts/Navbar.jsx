5e02a2cce72892c2eb296f59accb5251e2a3856d
idwan <idwanx@gmail.com>
2024-02-14 16:59:18 +0800

    wip

--- resources/js/Layouts/Navbar.jsx
+++ resources/js/Layouts/Navbar.jsx
@@ -198,12 +198,12 @@ export default function Navbar() {
                         null
                         }
                         
-                        <NavLink>
+                        {/* <NavLink>
                             Data Pendukung
                         </NavLink>
                         <NavLink>
                             Users
-                        </NavLink>
+                        </NavLink> */}
                     </div>
                     </div>
                     <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
