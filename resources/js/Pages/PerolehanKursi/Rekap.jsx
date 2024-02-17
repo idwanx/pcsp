05f5ff7f4cfec587b840048ce3699d56383c33ba
idwan <idwanx@gmail.com>
2024-02-17 15:42:01 +0800

    wip

--- resources/js/Pages/PerolehanKursi/Rekap.jsx
+++ resources/js/Pages/PerolehanKursi/Rekap.jsx
@@ -140,7 +140,7 @@ export default function Rekap() {
                                                             </div>
                                                             
                                                             <div className="min-w-0 flex-auto pt-4">
-                                                            <p className="text-ms text-rose-600">{items.length} Calon berdasarkan perengkingan suara terbanyak:</p>
+                                                            <p className="text-ms text-rose-500">{items.length} Calon berdasarkan perengkingan suara terbanyak:</p>
                                                                 <ul role="list" className="divide-y divide-gray-200">
                                                                 {items?.[0].calonpartai.slice(0,items.length).map((terpilih, i) => (
                                                                     <li key={i} className="flex py-2 items-center">
